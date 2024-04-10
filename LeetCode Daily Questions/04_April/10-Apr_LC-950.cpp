5f57028f526ff86beed17022107218c60ad8b0c6
dubeyayushi <adayushi232@gmail.com>
2024-04-10 13:22:13 +0530

    Create 10-Apr_LC-950.cpp

---
+++ LeetCode Daily Questions/04_April/10-Apr_LC-950.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/reveal-cards-in-increasing-order/description/?envType=daily-question&envId=2024-04-10
+
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        int n = deck.size();
+        sort(deck.begin(), deck.end());
+        queue<int>q;
+        for (int i = 0; i<n; i++){
+            q.push(i);
+        }
+        vector<int>ans(n);
+        for (int i = 0; i<n; i++){
+            int idx = q.front();
+            q.pop();
+            q.push(q.front());
+            q.pop();
+            ans[idx] = deck[i];
+        }
+        return ans;
+    }
+};
