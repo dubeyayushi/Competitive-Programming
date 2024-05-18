fca2aee5290fe228d670f5af6b310b955cf40721
dubeyayushi <adayushi232@gmail.com>
2024-05-18 13:01:44 +0530

    Create 18-May_LC-979.cpp

---
+++ LeetCode Daily Questions/05_May/18-May_LC-979.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/distribute-coins-in-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int moves = 0;
+
+    int dfs(TreeNode* node){
+        if (node==NULL) return 0;
+        int left_excess = dfs(node->left);
+        int right_excess = dfs(node->right);
+        moves += abs(left_excess) + abs(right_excess);
+        return node->val + left_excess + right_excess - 1;
+    }
+
+    int distributeCoins(TreeNode* root) {
+        dfs(root);
+        return moves;
+    }
+};
