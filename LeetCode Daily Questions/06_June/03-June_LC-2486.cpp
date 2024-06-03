ff93f9332101c94c3231d235888b679e0617aa7f
dubeyayushi <adayushi232@gmail.com>
2024-06-03 08:34:19 +0530

    Create 03-June_LC-2486.cpp

---
+++ LeetCode Daily Questions/06_June/03-June_LC-2486.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/description/
+
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int i = 0;
+        int j = 0;
+        while(i<s.length() && j<t.length()){
+            if (s[i]==t[j]){
+                j++;
+            }
+            i++;
+        }
+        return t.length() - j;
+    }
+};
